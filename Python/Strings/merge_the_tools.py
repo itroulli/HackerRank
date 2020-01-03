a6439c848051562fa3efe4176ce6781c0fa60b1e
Ilias Troullinos <htrul18@gmail.com>
2020-01-03 11:21:29 +0000

    Complete String

---
+++ Python/Strings/merge_the_tools.py
@@ -0,0 +1,19 @@
+def merge_the_tools(string, k):
+    t = []
+    u = []
+    s = set()
+    n = len(string)
+    for i in range(n // k):
+        t.append(string[i * k:(i + 1) * k])
+        for c in t[i]:
+            if c not in s:
+                s.add(c)
+                u.append(c)
+        print(''.join(u))
+        u = []
+        s = set()
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
