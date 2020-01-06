f38a4fab84b27158f44c72d2110181fc55f25467
Ilias Troullinos <htrul18@gmail.com>
2020-01-06 17:08:40 +0100

    Add prefix to Python Introduction

--- Python/Introduction/Python_Division.py
+++
@@ -1,8 +0,0 @@
-# Name: Python Division
-# Problem: https://www.hackerrank.com/challenges/python-division/problem
-# Score: 10
-
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(f'{a//b}\n{a/b}')
