2e69cf9493bb69483aba3b170a55c08dbfb83a47
Ilias Troullinos <htrul18@gmail.com>
2020-01-31 11:53:51 +0100

    Add Day 15 to '30 Days of Code'

---
+++ Tutorials/30_Days_of_Code/015-linked_list.py
@@ -0,0 +1,36 @@
+# Name: Day 15: Linked List
+# Problem: https://www.hackerrank.com/challenges/30-linked-list/problem
+# Score: 30
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def insert(self, head, data):
+        # Complete this method
+        if head is None:
+            head = Node(data)
+        elif head.next is None:
+            head.next = Node(data)
+        else:
+            self.insert(head.next, data)
+        return head
+
+
+mylist = Solution()
+T = int(input())
+head = None
+for i in range(T):
+    data = int(input())
+    head = mylist.insert(head, data)
+mylist.display(head)
